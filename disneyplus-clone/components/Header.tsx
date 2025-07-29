72a56e405bc190a3f4217cfa5256a2594a5cc9b4
Chris Nguyen <christopher.nguyen.mms@gmail.com>
2025-07-28 20:00:09 -0400

    Added TMDB API functionality, stopped at 1:27:07

--- disneyplus-clone/components/Header.tsx
+++ disneyplus-clone/components/Header.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import { ThemeToggler } from "./ThemeToggler";
+import SearchInput from "./SearchInput";
+import GenreDropdown from "./GenreDropdown";
 
 function Header() {
   return (
-    <header>
-      <Link href="/">
+    <header
+      className="fixed w-full z-20 top-0 flex items-center 
+    justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to to-gray-900"
+    >
+      <Link href="/" className="mr-10">
         <Image
           src="https://links.papareact.com/a943ae"
           alt="Disney Logo"
@@ -14,9 +19,9 @@ function Header() {
           className="cursor-pointer invert-0 dark:invert"
         />
       </Link>
-      <div className="flex space-x 2">
-        {/* GenreDropDown */}
-        {/* SearchInput */}
+      <div className="flex space-x-2">
+        <GenreDropdown />
+        <SearchInput />
         <ThemeToggler />
       </div>
     </header>
