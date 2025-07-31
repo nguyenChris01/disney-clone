7709857f0ea35d05ceb1472ec7e585dd88ad40b8
Chris Nguyen <christopher.nguyen.mms@gmail.com>
2025-07-30 22:59:10 -0400

    Adding Azure Functions for OPENAPI functionality, stopped at 2:47:22

---
+++ disneyplus-clone/function/src/functions/getAISuggestion.ts
@@ -0,0 +1,29 @@
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+
+import OpenAI from "openai";
+
+const openAI = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function getAISuggestion(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  const name = request.query.get("name") || (await request.text()) || "world";
+
+  return { body: `Hello, ${name}!` };
+}
+
+app.http("getAISuggestion", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: getAISuggestion,
+});
